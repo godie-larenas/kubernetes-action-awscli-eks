b0a39f1f3cd7ef08d46b4b9ccb009b1c54e8f3b7
godie-larenas <136631610+godie-larenas@users.noreply.github.com>
2023-08-08 17:56:08 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,6 @@ export KUBECONFIG=kubeconfig
 result="$(kubectl $1)"
 
 status=$?
-echo ::set-output name=result::$result
-echo "$result"
+echo "{name}={value}" >> $GITHUB_OUTPUT
+echo "$GITHUB_OUTPUT"
 if [[ $status -eq 0 ]]; then exit 0; else exit 1; fi
